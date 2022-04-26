ddaaa0099b54a4d862f1150158fced34b2dc7ffb
maxxcleiton~ <maxcleiton@live.com>
2022-04-25 23:53:55 -0300

    alteracoes importantes - projeto pronto

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const __dirname = path.resolve(path.dirname('')) // __dirname serve para informa
 
 console.log(__dirname)
 const app = express() // instanciando o express dentro da const app
+app.use(express.urlencoded({extended: true})) // Informacao vai para o body (o corpo da requisicao)
+app.use(express.json())
 
 app.set("view engine", "ejs") // express reconheça o ejs da pasta views
 app.use(express.static(path.join(__dirname, "public")))
@@ -19,40 +21,39 @@ let message = "";
 
 let pokedex = [
     {
-        Id: 1,
-        Nome: 'Pikachu',
-        Tipo: 'Elétrico',
-        Imagem: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png',
-        Descrição: 'Pikachu que pode gerar eletricidade poderosa tem bolsas nas bochechas que são extra macias e super elásticas.',
-        Altura: 1.4,
-        Peso: 6,
-        Categoria: 'Rato Elétrico',
-        Habilidade: 'Estático',
+        id: 1,
+        nome: 'Pikachu',
+        tipo: 'Elétrico',
+        imagem: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/025.png',
+        descricao: 'Pikachu que pode gerar eletricidade poderosa tem bolsas nas bochechas que são extra macias e super elásticas.',
+        altura: 1.4,
+        peso: 6,
+        categoria: 'Rato Elétrico',
+        habilidade: 'Estático',
     }, 
     {
-        Id: 2,
-        Nome: 'Charmander',
-        Tipo: 'Fogo',
-        Imagem: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
-        Descrição: 'Tem preferência por coisas quentes. Quando chove, diz-se que o vapor jorra da ponta de sua cauda.',
-        Altura: 0.6,
-        Peso: 8.5,
-        Categoria: 'Lagarto',
-        Habilidade: 'Chama',
+        id: 2,
+        nome: 'Charmander',
+        tipo: 'Fogo',
+        imagem: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
+        descricao: 'Tem preferência por coisas quentes. Quando chove, diz-se que o vapor jorra da ponta de sua cauda.',
+        altura: 0.6,
+        peso: 8.5,
+        categoria: 'Lagarto',
+        habilidade: 'Chama',
     }, 
     {
-        Id: 3,
-        Nome: 'Squirtle',
-        Tipo: 'Água',
-        Imagem: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png',
-        Descrição: 'Quando retrai seu longo pescoço em sua concha, esguicha água com força vigorosa.',
-        Altura: 0.5,
-        Peso: 9,
-        Categoria: 'Tartaruga',
-        Habilidade: 'Corrente de água',
-    }
+        id: 3,
+        nome: 'Squirtle',
+        tipo: 'Água',
+        imagem: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png',
+        descricao: 'Quando retrai seu longo pescoço em sua concha, esguicha água com força vigorosa.',
+        altura: 0.5,
+        peso: 9,
+        categoria: 'Tartaruga',
+        habilidade: 'Corrente de água',
+    },
 ];
-
 //
 
 app.get('/', (req, res) => { // get é um método http/https que serve para trazer uma pagina
@@ -64,7 +65,7 @@ app.get('/', (req, res) => { // get é um método http/https que serve para traz
 app.get('/detalhes/:id', (req, res) => { // get é um método http/https que serve para trazer uma pagina
     let poke = []
     pokedex.filter((element) => {
-        if(element.Id == req.params.id) {
+        if(element.id == req.params.id) {
             poke.push(element)
         }
     })
@@ -76,9 +77,9 @@ app.get('/detalhes/:id', (req, res) => { // get é um método http/https que ser
     })
 })
 
-app.get('/cadastro/', (req, res) => { // get é um método http/https que serve para trazer uma pagina
+app.get('/cadastro', (req, res) => { // get é um método http/https que serve para trazer uma pagina
     res.render('cadastro.ejs', {
-        pokedex,
+        // pokedex,
         message,
     })
 })
@@ -91,13 +92,25 @@ app.get('/', (req,res) => {
 });
 
 // POST METHOD ROUTE
-app.post('/', (req,res) => {
-    res.send('POST request to the homepage')
+app.post('/cadastro', (req,res) => {
+    let i = pokedex[pokedex.length-1].id + 1
+
+    const { nome, tipo, imagem, descricao, altura, peso, categoria, habilidade, } = req.body;
+    pokedex.push({ id: i, nome, tipo, imagem, descricao, altura, peso, categoria, habilidade,})
+    console.log(pokedex)
+    // res.send({ id: i, nome: nome, tipo: tipo, imagem: imagem, descricao: descricao, altura: altura, peso: peso, categoria: categoria, habilidade: habilidade})
+
+    // res.send({ nome, tipo, imagem, descricao, altura, peso, categoria, habilidade })
+
+    // pokedex.push({ Id: i, Nome, Tipo, Imagem, Descrição, Altura, Peso, Categoria, Habilidade}, req.body)
+    // pokedex.push({Id: i}, req.body)
+    // console.log(pokedex)
+    res.redirect("/");
 });
 
 
 // Isso aqui sempre antes do post mais pra frente
-app.use(express.urlencoded());
+// app.use(express.urlencoded());
 
 // Tudo do formulário eu preciso organizar o json:
 // app.post('/subscription', (req,res) => {
@@ -106,18 +119,18 @@ app.use(express.urlencoded());
 // });
 
 // Aqui é a versao adicionando mensagem estilizada de resposta
-app.post('/subscription', (req, res) => {
-    const { nome, tipo, imagem, descricao, altura, peso, categoria, habilidade } = req.body;
-    // res.send({
-    //     nome: nome,
-    //     tipo: tipo,
-    //     imagem: imagem,
-    //     descricao: descricao,
-    //     altura: altura,
-    //     peso: peso,
-    //     categoria: categoria,
-    //     habilidade: habilidade,
-    // })
-    message = `Pokémon cadastrado com sucesso!`;
-    res.redirect('/');
-});
+// app.post('/subscription', (req, res) => {
+//     const { nome, tipo, imagem, descricao, altura, peso, categoria, habilidade } = req.body;
+//     // res.send({
+//     //     nome: nome,
+//     //     tipo: tipo,
+//     //     imagem: imagem,
+//     //     descricao: descricao,
+//     //     altura: altura,
+//     //     peso: peso,
+//     //     categoria: categoria,
+//     //     habilidade: habilidade,
+//     // })
+//     message = `Pokémon cadastrado com sucesso!`;
+//     res.redirect('/');
+// });
